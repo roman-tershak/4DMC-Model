486a39d5d12c22920c13aa36930b04309398bcdd
Roman Tershak <roman.tershak@sethq.com>
2014-07-11 18:04:57 +0300

    Working on NeoPixel

---
+++ Firmware/central_controller/neopixel.h
@@ -0,0 +1,20 @@
+#ifndef H_NEOPIXEL
+#define H_NEOPIXEL
+
+#include <avr/io.h>
+#include <stdio.h>
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+typedef volatile uint8_t* port8_addr_t;
+typedef uint8_t port_pin_t;
+
+/* Hardware NeoPixel LED Functions */
+
+#ifdef __cplusplus
+}
+#endif 
+
+#endif
